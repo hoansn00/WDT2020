6207470f91520eff6d95564d7928f4f2a6432859
Phạm Văn Hoan <61729888+hoansn00@users.noreply.github.com>
2020-12-11 00:14:14 +0700

    commit

---
+++ PhamVanHoan-0116/MISA.CukCuk.Web/js/page/customer.js
@@ -0,0 +1,12 @@
+﻿$(document).ready(function(){
+    // b1 goij service lấy dữ liệu:( api.manhnv.net/api/customers)
+    $.ajax({
+        url: 'http://api.manhnv.net/api/customers',
+        method: 'GET',
+        async: false
+    }).done(function (response) {
+        console.log(response);
+    }).fail(function(response){
+
+    })
+})
